02b39707fdb57125cc1da497666970cdc8d28fef
Roan Kattouw <roan.kattouw@gmail.com>
2011-06-18 13:57:39 +0200

    Excercise 3

--- session3/ex3.c
+++ session3/ex3.c
@@ -13,7 +13,7 @@ void *page;
 int readFD, writeFD;
 pid_t otherPID;
 int myNum;
-int havePage;
+int dirty; /* 0: both up-to-date, 1: mine up-to-date and dirty, 2: theirs up-to-date and dirty */
 
 void segvHandler(int sig, siginfo_t *info, void *context) {
 	size_t bytesRead, bytesLeft;
@@ -25,30 +25,39 @@ void segvHandler(int sig, siginfo_t *info, void *context) {
 		return;
 	}
 	
-	if(havePage)
+	if(dirty == 0)
 	{
-		puts("WTF");
-		raise(SIGHUP);
+		/* Trying to write to a clean page */
+		/* Mark the page as dirty and ours */
+		dirty = 1;
+		/* Inform the other process the page is dirty */
+		kill(otherPID, SIGUSR2);
+		/* Unprotect the page */
+		mprotect(page, PAGESIZE, PROT_READ | PROT_WRITE);
 	}
-	/* Obtain the page */
-	/* Signal the other process */
-	kill(otherPID, SIGUSR1);
-	/* Unprotect the page, must happen before reading it in */
-	mprotect(page, PAGESIZE, PROT_WRITE | PROT_READ);
-	/* Read the page from the pipe */
-	bytesRead = 0;
-	bytesLeft = PAGESIZE;
-	p = page;
-	do {
-		bytesRead = read(readFD, p + PAGESIZE - bytesLeft, bytesLeft);
-		if(bytesRead < 0)
-		{
-			perror("read");
-			exit(errno);
-		}
-		bytesLeft -= bytesRead;
-	} while(bytesLeft > 0);
-	havePage = 1;
+	else if(dirty == 2)
+	{
+		/* Obtain the page */
+		/* Signal the other process */
+		kill(otherPID, SIGUSR1);
+		/* Unprotect the page, must happen before reading it in */
+		mprotect(page, PAGESIZE, PROT_READ | PROT_WRITE);
+		/* Read the page from the pipe */
+		bytesRead = 0;
+		bytesLeft = PAGESIZE;
+		p = page;
+		do {
+			bytesRead = read(readFD, p + PAGESIZE - bytesLeft, bytesLeft);
+			if(bytesRead < 0)
+			{
+				perror("read");
+				exit(errno);
+			}
+			bytesLeft -= bytesRead;
+		} while(bytesLeft > 0);
+		mprotect(page, PAGESIZE, PROT_READ);
+		dirty = 0;
+	}
 }
 
 void usr1Handler(int sig)
@@ -56,11 +65,6 @@ void usr1Handler(int sig)
 	size_t bytesWritten, bytesLeft;
 	char *p;
 	
-	if(!havePage)
-	{
-		puts("WTF2");
-		raise(SIGHUP);
-	}
 	/* The other process wants the page */
 	/* Write it to the pipe */
 	bytesWritten = 0;
@@ -76,14 +80,58 @@ void usr1Handler(int sig)
 		bytesLeft -= bytesWritten;
 	} while(bytesLeft > 0);
 	/* Protect the page. Must happen after writing it out */
+	mprotect(page, PAGESIZE, PROT_READ);
+	dirty = 0;
+}
+
+void usr2Handler(int sig)
+{
+	/* The other process is writing to the page */
+	dirty = 2;
 	mprotect(page, PAGESIZE, PROT_NONE);
-	havePage = 0;
 }
 
 int main(void) {
 	int ok, child, i, status, fds[4];
-	struct sigaction segv, usr1;
+	struct sigaction segv, usr1, usr2;
 	int *turn;
+	
+	/* Allocate buffer */
+	ok = posix_memalign(&page, PAGESIZE, PAGESIZE);
+	if (ok != 0) {
+		perror("Couldn't malloc an aligned page");
+		exit(errno);
+	}
+	
+	/* Initialize stuff */
+	turn = page;
+	*turn = 0;
+	dirty = 0;
+	mprotect(page, PAGESIZE, PROT_READ);
+	
+	/* Install SIGSEGV handler */
+	segv.sa_sigaction = segvHandler;
+	segv.sa_flags = SA_SIGINFO | SA_RESTART;
+	sigemptyset(&segv.sa_mask);
+	sigaddset(&segv.sa_mask, SIGUSR1);
+	sigaddset(&segv.sa_mask, SIGUSR2);
+	sigaction(SIGSEGV, &segv, NULL);
+	
+	/* Install SIGUSR1 handler */
+	usr1.sa_handler = usr1Handler;
+	usr1.sa_flags = SA_RESTART;
+	sigemptyset(&usr1.sa_mask);
+	sigaddset(&usr1.sa_mask, SIGSEGV);
+	sigaddset(&usr1.sa_mask, SIGUSR2);
+	sigaction(SIGUSR1, &usr1, NULL);
+	
+	/* Install SIGUSR2 handler */
+	usr2.sa_handler = usr2Handler;
+	usr2.sa_flags = SA_RESTART;
+	sigemptyset(&usr2.sa_mask);
+	sigaddset(&usr2.sa_mask, SIGSEGV);
+	sigaddset(&usr2.sa_mask, SIGUSR1);
+	sigaction(SIGUSR2, &usr2, NULL);
 	
 	/* Set up a pipe and fork */
 	pipe(fds);
@@ -113,50 +161,14 @@ int main(void) {
 		/* Child */
 		readFD = fds[2];
 		writeFD = fds[1];
-		close(fds[0]);
 		close(fds[3]);
-	}
-	
-	/* Install SIGSEGV handler */
-	segv.sa_sigaction = segvHandler;
-	segv.sa_flags = SA_SIGINFO | SA_RESTART;
-	sigemptyset(&segv.sa_mask);
-	sigaddset(&segv.sa_mask, SIGUSR1);
-	sigaction(SIGSEGV, &segv, NULL);
-	
-	/* Install SIGUSR1 handler */
-	usr1.sa_handler = usr1Handler;
-	usr1.sa_flags = SA_RESTART;
-	sigemptyset(&usr1.sa_mask);
-	sigaddset(&usr1.sa_mask, SIGSEGV);
-	sigaction(SIGUSR1, &usr1, NULL);
-	
-	/* Allocate buffer */
-	ok = posix_memalign(&page, PAGESIZE, PAGESIZE);
-	if (ok != 0) {
-		perror("Couldn't malloc an aligned page");
-		exit(errno);
-	}
-	
-	/* Initially, the parent owns the page */
-	turn = page;
-	if(myNum)
-	{
-		mprotect(page, PAGESIZE, PROT_NONE);
-		havePage = 0;
-	}
-	else
-	{
-		mprotect(page, PAGESIZE, PROT_READ | PROT_WRITE);
-		*turn = myNum;
-		havePage = 1;
 	}
 	
 	for(i = 0; i < 100; i++)
 	{
 		
 		while(*turn != myNum);
-		printf("%d: %s\n", getpid(), myNum == 1 ? "Pong" : "Ping");
+		printf("%d: %s #%d\n", getpid(), myNum == 1 ? "Pong" : "Ping", i);
 		*turn = !myNum;
 	}
 	
@@ -164,8 +176,8 @@ int main(void) {
 	if(child)
 	{
 		signal(SIGUSR1, SIG_IGN);
-		if(havePage)
-			usr1Handler(0);
+		signal(SIGUSR2, SIG_IGN);
+		usr1Handler(0);
 		waitpid(child, &status, 0);
 	}
 	
