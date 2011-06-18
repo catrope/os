63c0789d891b2c763092751ef12bc9479a6754c6
Roan Kattouw <roan.kattouw@gmail.com>
2011-06-18 15:49:24 +0200

    Reorganize ex2 to bring it in line with ex3, and kill havePage

--- session3/ex2.c
+++ session3/ex2.c
@@ -13,7 +13,6 @@ void *page;
 int readFD, writeFD;
 pid_t otherPID;
 int myNum;
-int havePage;
 
 void segvHandler(int sig, siginfo_t *info, void *context) {
 	size_t bytesRead, bytesLeft;
@@ -25,11 +24,6 @@ void segvHandler(int sig, siginfo_t *info, void *context) {
 		return;
 	}
 	
-	if(havePage)
-	{
-		puts("WTF");
-		raise(SIGHUP);
-	}
 	/* Obtain the page */
 	/* Signal the other process */
 	kill(otherPID, SIGUSR1);
@@ -48,7 +42,6 @@ void segvHandler(int sig, siginfo_t *info, void *context) {
 		}
 		bytesLeft -= bytesRead;
 	} while(bytesLeft > 0);
-	havePage = 1;
 }
 
 void usr1Handler(int sig)
@@ -56,11 +49,6 @@ void usr1Handler(int sig)
 	size_t bytesWritten, bytesLeft;
 	char *p;
 	
-	if(!havePage)
-	{
-		puts("WTF2");
-		raise(SIGHUP);
-	}
 	/* The other process wants the page */
 	/* Write it to the pipe */
 	bytesWritten = 0;
@@ -77,13 +65,37 @@ void usr1Handler(int sig)
 	} while(bytesLeft > 0);
 	/* Protect the page. Must happen after writing it out */
 	mprotect(page, PAGESIZE, PROT_NONE);
-	havePage = 0;
 }
 
 int main(void) {
 	int ok, child, i, status, fds[4];
 	struct sigaction segv, usr1;
 	int *turn;
+	
+	/* Allocate buffer */
+	ok = posix_memalign(&page, PAGESIZE, PAGESIZE);
+	if (ok != 0) {
+		perror("Couldn't malloc an aligned page");
+		exit(errno);
+	}
+	
+	/* Initialize stuff */
+	turn = page;
+	*turn = 0;
+	
+	/* Install SIGSEGV handler */
+	segv.sa_sigaction = segvHandler;
+	segv.sa_flags = SA_SIGINFO | SA_RESTART;
+	sigemptyset(&segv.sa_mask);
+	sigaddset(&segv.sa_mask, SIGUSR1);
+	sigaction(SIGSEGV, &segv, NULL);
+	
+	/* Install SIGUSR1 handler */
+	usr1.sa_handler = usr1Handler;
+	usr1.sa_flags = SA_RESTART;
+	sigemptyset(&usr1.sa_mask);
+	sigaddset(&usr1.sa_mask, SIGSEGV);
+	sigaction(SIGUSR1, &usr1, NULL);
 	
 	/* Set up a pipe and fork */
 	pipe(fds);
@@ -117,40 +129,11 @@ int main(void) {
 		close(fds[3]);
 	}
 	
-	/* Install SIGSEGV handler */
-	segv.sa_sigaction = segvHandler;
-	segv.sa_flags = SA_SIGINFO | SA_RESTART;
-	sigemptyset(&segv.sa_mask);
-	sigaddset(&segv.sa_mask, SIGUSR1);
-	sigaction(SIGSEGV, &segv, NULL);
-	
-	/* Install SIGUSR1 handler */
-	usr1.sa_handler = usr1Handler;
-	usr1.sa_flags = SA_RESTART;
-	sigemptyset(&usr1.sa_mask);
-	sigaddset(&usr1.sa_mask, SIGSEGV);
-	sigaction(SIGUSR1, &usr1, NULL);
-	
-	/* Allocate buffer */
-	ok = posix_memalign(&page, PAGESIZE, PAGESIZE);
-	if (ok != 0) {
-		perror("Couldn't malloc an aligned page");
-		exit(errno);
-	}
-	
 	/* Initially, the parent owns the page */
-	turn = page;
-	if(myNum)
-	{
-		mprotect(page, PAGESIZE, PROT_NONE);
-		havePage = 0;
-	}
-	else
-	{
+	if(myNum == 0)
 		mprotect(page, PAGESIZE, PROT_READ | PROT_WRITE);
-		*turn = myNum;
-		havePage = 1;
-	}
+	else
+		mprotect(page, PAGESIZE, PROT_NONE);
 	
 	for(i = 0; i < 100; i++)
 	{
@@ -161,11 +144,10 @@ int main(void) {
 	}
 	
 	/* Parent hands over the page and waits for child */
-	if(child)
+	if(myNum == 0)
 	{
 		signal(SIGUSR1, SIG_IGN);
-		if(havePage)
-			usr1Handler(0);
+		usr1Handler(0);
 		waitpid(child, &status, 0);
 	}
 	
